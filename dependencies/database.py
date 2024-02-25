b9de489f17a3cc45b2a6d9a4b7fd691866cc3268
Amir Hossein Mohammadi <101041110+amirhnmi@users.noreply.github.com>
2024-02-25 17:01:15 +0330

    h

--- dependencies/database.py
+++ dependencies/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+mysqlconnector://root:manamirhastam@127.0.0.1:3306/dornica_test_db")
+engine = create_engine("mysql+mysqlconnector://***:***@127.0.0.1:3306/***")
 sessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
